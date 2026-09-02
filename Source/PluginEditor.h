e48802e27850dde3330dcbfb9cbb848918ed6e62
Shahriar <shahriaralrafi7070@gmail.com>
2026-09-02 17:16:29 +0600

    Give Editer  it's own monchain

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -59,5 +59,8 @@ private:
 
     std::vector<juce::Component*> getComs();
 
+	MonoChain monoChain;
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEQAudioProcessorEditor);
 };
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -26,6 +26,20 @@ struct ChainSettings
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
+using Filter = juce::dsp::IIR::Filter<float>;
+
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
 //==============================================================================
 /**
 */
@@ -73,22 +87,9 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this,nullptr,"Parameters",createParameterLayout() };
 
 private:
-
-	using Filter = juce::dsp::IIR::Filter<float>;
-
-	using CutFilter = juce::dsp::ProcessorChain<Filter, Filter,Filter,Filter>;
-
-	using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
-
+	
 	MonoChain leftChain, rightChain;
 
-    enum ChainPositions
-    {
-		LowCut,
-		Peak,
-		HighCut
-    };
-    
 	void updatePeakFilter(const ChainSettings& chainSettings);
 	using Coefficients = Filter::CoefficientsPtr;
 	static void updateCoefficients(Coefficients& old, const Coefficients& replacements); 
