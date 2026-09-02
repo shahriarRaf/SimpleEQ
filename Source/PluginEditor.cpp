944d73cf84d9b13413030086d271f57d5dccc17a
Shahriar <shahriaralrafi7070@gmail.com>
2026-09-02 23:08:31 +0600

    Gui Listens from params changes now

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,11 +28,24 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
 	{
 		addAndMakeVisible(comp);
 	}
+
+	startTimer(60);
     setSize (600, 400);
+
+	const auto& params = audioProcessor.getParameters();
+	for (auto param : params)
+	{
+		param->addListener(this);
+	}
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+	const auto& params = audioProcessor.getParameters();
+	for (auto param : params)
+	{
+		param->removeListener(this);
+	}
 }
 
 //==============================================================================
@@ -147,8 +160,15 @@ void SimpleEQAudioProcessorEditor::timerCallback()
 {
 	if ( parametersChanged.compareAndSetBool(false, true))
 	{
+
+		DBG("parameter changed");
 		//update the monochain
+		auto chainSettings = getChainSettings(audioProcessor.apvts);
+		auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+		updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+		 
 		//signal a repaint
+		repaint();
 	}
 }
 
